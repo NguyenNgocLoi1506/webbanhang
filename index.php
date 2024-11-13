58c3a1422d30f048da1eda684a82311543caccd9
NguyenNgocLoi1506 <2251120028@ut.edu.vn>
2024-11-13 20:44:38 +0700

    webbh

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>web bán bia</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <img src="images/banner.jpg" width="100%" height="180px">
+        </div>
+        <div class="menu">
+            <ul class="list menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+                <ul class="list sidebar">
+                    <li><a href="#">Bia Việt</a></li>
+                    <li><a href="#">Bia Huda</a></li>
+                    <li><a href="#">Bia Saigon Special</a></li>
+                    <li><a href="#">Bia Tiger Soju Vị Dưa Lưới</a></li>
+                    <li><a href="#">Bia Ha Noi BOLD</a></li>
+                    <li><a href="#">Bia Heineken</a></li>
+                    <li><a href="#">Bia Larue Smooth </a></li>
+                    <li><a href="#">Bia Ha Noi Premium</a></li>
+                    <li><a href="#">Bia Ha Noi Light</a></li>
+                    <li><a href="#">Bia Heineken Silver</a></li>
+                    <li><a href="#">Bia 333</a></li>
+                    <li><a href="#">Bia Tiger</a></li>
+                </ul>
+            </div>
+
+            <div class="maincontent">
+                <ul class="product list">
+                    <li>
+                        <a href="">
+                            <img src="images/biaviet.jpg" width="100%">
+                            <p class="title product"> Bia Việt</p>
+                                <p class="price product ">12.000 đồng</p>
+                        </a>
+                    </li>
+                    <li>
+                        <img src="images/biahuda.jpg" width="100%">
+                        <p class="title product">Bia Huda</p>
+                            <p class="price product "> 2.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biasaigonspecial.jpg" width="100%">
+                        <p class="title product">Bia Saigon Special</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biatigersoju.webp" width="100%">
+                        <p class="title product">Bia Tiger Soju Vị Dưa Lưới</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biahanoibold.jpg" width="100%">
+                        <p class="title product">Bia Ha Noi BOLD</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biaheineken.jpg" width="100%">
+                        <p class="title product">Bia Heineken</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/bialauramooth.jpg" width="100%">
+                        <p class="title product">Bia Larue Smooth</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biahanoipremium.png" width="100%">
+                        <p class="title product">Bia Ha Noi Premium</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biahanoilight.jpg" width="100%">
+                        <p class="title product">Bia Ha Noi Light</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biaheinekensilver.jpg" width="100%">
+                        <p class="title product">Bia Heineken Silver</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/bia333.jpg" width="100%">
+                        <p class="title product">Bia 333</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                    <li>
+                        <img src="images/biatiger.jpg" width="100%">
+                        <p class="title product">Bia Tiger</p>
+                            <p class="price product ">12.000 đồng</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p class="footer copyright">copyright by QA&NL</p>
+        </div>
+    </div>
+</body>
+</html>
